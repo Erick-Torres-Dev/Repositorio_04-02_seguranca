d33b9ae50ea98c26804e53ee353974751441950b
Erick <erichktorres@gmail.com>
2026-02-04 20:53:38 -0300

    rapaz...acho que é o 7

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/landing_page/',
+  base: '/Repositorio_04-02_seguranca/',
   plugins: [react()],
   server: {
     port: 3000,
